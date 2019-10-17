743aacd6b754b1743b7f6adef9fa22da09182558
ivominic <ivo.minic@gmail.com>
2019-10-17 15:02:26 +0200

    Dodatno... Nastaviti u projektu iis, na odvojenoj grani

--- grails-app/domain/g4test/workflowProces/Korak.groovy
+++ grails-app/domain/g4test/workflowProces/Korak.groovy
@@ -6,9 +6,18 @@ class Korak {
     String naziv
     Integer redniBrojKoraka
     Boolean generisanjeNovogDokumenta = false
+    Proces noviProces //Ili bi ovdje trebalo da ukazuje na drugi korak
+    String kontroler = ""
+    String metoda = ""
+    //Grupe korisnika kojima se prosljeđuje dokument
+    //Atributi koji moraju biti popunjeni
+    //Vrijednosti atributa od kojih zavisi granjanje, tj kad će se preći na koji korak/proces da bude navedeno u ovoj vezi/klasi
 
 
     static constraints = {
+        noviProces nullable: true //ili postaviti da je novi proces isti proces ako se ne mijenja
+        kontroler nullable: true, blank: true
+        metoda nullable: true, blank: true
     }
 
     String toString(){
