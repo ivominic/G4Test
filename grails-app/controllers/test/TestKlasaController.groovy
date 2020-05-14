e871a171e83b5dd28b1b6b28c393e526f57ed413
Ivo Minic <ivo.minic@gmail.com>
2020-05-14 14:58:23 +0200

    Kreiran OCR

--- grails-app/controllers/test/TestKlasaController.groovy
+++ grails-app/controllers/test/TestKlasaController.groovy
@@ -1,5 +1,9 @@
 package test
 
+import net.sourceforge.tess4j.Tesseract
+import net.sourceforge.tess4j.Tesseract1
+import net.sourceforge.tess4j.TesseractException
+
 
 class TestKlasaController {
 
@@ -9,6 +13,32 @@ class TestKlasaController {
 
     def gridStore(){
         return []
+    }
+
+    def ocr(){
+        Tesseract tesseract = new Tesseract()
+        //Tesseract1 tesseract = new Tesseract1()
+        try {
+
+            tesseract.setDatapath("C:/dms/")
+            //tesseract.setLanguage("srp_latin")
+
+            println tesseract.getProperties()
+
+            // the path of your tess data folder
+            // inside the extracted file
+            //String text = tesseract.doOCR(new File("test.png"))
+            //String text = tesseract.doOCR(new File("C:/dms/test1.png"))
+            String text = tesseract.doOCR(new File("C:/dms/tabela.pdf"))
+
+            // path of your image file
+            println text
+            render text
+        }
+        catch (TesseractException e) {
+            println e
+            render e
+        }
     }
 
 }
